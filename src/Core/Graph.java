c146b7acf7422f3344760a61ef7e06f9971fd32a
RomeoFroger49 <romeo.froger@gmail.com>
2025-10-14 15:48:33 +0200

    add methods to retrieve vertices and edges in Graph class

--- src/Core/Graph.java
+++ src/Core/Graph.java
@@ -6,4 +6,30 @@ public class Graph {
     String label;
 
     public Graph(Vertex[] vertices, Edge[] edges, String label) {}
+
+    public Vertex[] getVertices() {
+        return vertices;
+    }
+
+    public Edge[] getEdges() {
+        return edges;
+    }
+
+    public Edge getEdge(Vertex v1, Vertex v2) {
+        for (Edge e : edges) {
+            if ((e.start == v1 && e.end == v2) || (!e.directed && e.start == v2 && e.end == v1)) {
+                return e;
+            }
+        }
+        return null;
+    }
+
+    public Vertex getVertex(Vertex v) {
+        for (Vertex vertex : vertices) {
+            if (vertex == v) {
+                return vertex;
+            }
+        }
+        return null;
+    }
 }

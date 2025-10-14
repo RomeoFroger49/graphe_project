dff4091ae512f8347b4db2412d77d1a99cd231f9
RomeoFroger49 <romeo.froger@gmail.com>
2025-10-14 22:57:22 +0200

    fix: small change

--- src/Algorithm/BFS.java
+++ src/Algorithm/BFS.java
@@ -8,53 +8,25 @@ import java.util.*;
 
 public class BFS {
 
-    public static ArrayList<Vertex> non_oriented(Graph g, Vertex start) {
+    @SuppressWarnings("unused")
+    public static  List<Vertex> bfsDirected(Graph g, Vertex start) {
+
         Queue<Vertex> queue = new LinkedList<>();
-        ArrayList<Vertex> path = new ArrayList<>();
-        Set<Vertex> visited = new HashSet<>();
+        LinkedHashSet<Vertex> visited = new LinkedHashSet<>();
 
         visited.add(start);
         queue.add(start);
-        while (!queue.isEmpty()) {
 
+        while (!queue.isEmpty()) {
             Vertex v = queue.poll();
-            path.add(v);
 
             for (Edge e : v.getEdges()) {
-
-                Vertex w = (e.getStart() == v) ? e.getEnd() : e.getStart();
-
+                Vertex w = e.getEnd();
                 if (visited.add(w)) {
                     queue.add(w);
                 }
             }
         }
-        return path;
-    }
-
-    public static  ArrayList<Vertex> oriented(Graph g, Vertex start) {
-
-        Queue<Vertex> queue = new LinkedList<>();
-        ArrayList<Vertex> path = new ArrayList<>();
-
-        start.setVisited();
-        queue.add(start);
-
-        while (!queue.isEmpty()) {
-
-            Vertex v = queue.poll();
-            path.add(v);
-
-            for (Edge e : v.getEdges()) {
-                if (e.getEnd() == v) continue;
-
-                Vertex toVisit = e.getEnd();
-                if (e.getStart() == v && !toVisit.isVisited()) {
-                    toVisit.setVisited();
-                    queue.add(toVisit);
-                }
-            }
-        }
-        return path;
+        return visited.stream().toList();
     }
 }
